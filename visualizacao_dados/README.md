ba99fd50fb2f1f1cd6ae8c7a05e24f3292250b43
FSLancelotte <fslancelotte@gmail.com>
2026-02-15 22:42:49 -0300

    Aula: Exemplos de Gráficos

--- visualizacao_dados/README.md
+++ visualizacao_dados/README.md
@@ -10,6 +10,12 @@ Estudo voltado na biblioteca matplotlib, realizando a plotagem de gráficos
 de barras, pizza e dispersão
 - **uso_seaborn.py:**
 Estudo voltado na biblioteca seaborn, realizando a plotagem de gráficos
+- **exemplos_graficos.py:**
+Aplica técnicas de visualização de dados para realizar o agrupamento
+de dados e analisar a contagem de diferentes categorias
 - **clientes-v3-preparados.csv:**
 Arquivo csv que contém os dados a serem utilizados para geração de gráficos
 nos scripts python
+- **meu_grafico.png:**
+Arquivo de imagem salvo a partir da plotagem de gráfico no script 
+*"exemplos_graficos.py"*
---
+++ visualizacao_dados/exemplos_graficos.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('clientes-v3-preparado.csv')
+
+df_corr = df[['salario', 'idade', 'anos_experiencia', 'nivel_educacao_cod', 'area_atuacao_cod', 'estado_cod']].corr()
+# Heatmap da correlação
+plt.figure(figsize=(10,8))
+sns.heatmap(df_corr, annot=True, fmt='.2f')
+plt.title('Mapa de Calor da Correlação entre Variáveis')
+plt.show()
+
+# Countplot
+sns.countplot(x='estado_civil', data=df)
+plt.title('Distribuição do Estado de Civil')
+plt.xlabel('Estado Civil')
+plt.ylabel('Contagem')
+plt.show()
+
+# Countplot com legenda
+sns.countplot(x='estado_civil', hue='nivel_educacao', data=df)
+plt.title('Distribuição do Estado de Civil')
+plt.xlabel('Estado Civil')
+plt.ylabel('Contagem')
+plt.legend(title='Nível de Educação')
+# Salvando o gráfico
+plt.savefig('meu_grafico.png')
+plt.show()
+
+
